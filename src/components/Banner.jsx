7f33e9af783cc1f072258ff14a07ff739334e7fe
sami irhamnillah <samiirhamnillah03@gmai.com>
2023-12-29 01:28:11 +0700

    first commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import bannerImg from "/images/home/banner1.png";
+
+const Banner = () => {
+  return (
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%">
+      <div className="py-24 flex flex-col md:flex-row-reverse items-center justify-between gap-8">
+        {/* img */}
+        <div className="md:w-1/2">
+          <img src={bannerImg} alt="" />
+          <div className="flex flex-col md:flex-row items-center justify-around -mt-14 gap-4">
+            <div className="bg-white px-3 py-2 rounded-2xl flex items-center gap-3 shadow-sm w-64">
+              <img
+                src="/images/home/Whiskas1.png"
+                alt=""
+                className="rounded-2xl"
+              />
+              <div className="space-y-1">
+                <h5>Whiskas</h5>
+                <div className="rating rating-sm">
+                  <input
+                    type="radio"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-500"
+                    readOnly
+                  />
+                  <input
+                    type="radio"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-500"
+                    readOnly
+                  />
+                  <input
+                    type="radio"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-500"
+                    checked
+                    readOnly
+                  />
+                  <input
+                    type="radio"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-400"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-400"
+                    readOnly
+                  />
+                </div>
+                <p className="text-red">$18.00</p>
+              </div>
+            </div>
+            <div className="bg-white px-3 py-2 rounded-2xl md:flex items-center gap-3 shadow-sm w-64 hidden">
+              <img
+                src="/images/home/Whiskas1.png"
+                alt=""
+                className="rounded-2xl"
+              />
+              <div className="space-y-1">
+                <h5>Whiskas</h5>
+                <div className="rating rating-sm">
+                  <input
+                    type="radio"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-500"
+                    readOnly
+                  />
+                  <input
+                    type="radio"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-500"
+                    readOnly
+                  />
+                  <input
+                    type="radio"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-500"
+                    checked
+                    readOnly
+                  />
+                  <input
+                    type="radio"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-400"
+                    readOnly
+                  />
+                  <input
+                    type="radio"
+                    name="rating-6"
+                    className="mask mask-star-2 bg-orange-400"
+                    readOnly
+                  />
+                </div>
+                <p className="text-red">$18.00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* texts */}
+        <div className="md:w-1/2 px-4 space-y-7">
+          <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
+            Indulge in the Delightful World of{" "}
+            <span className="text-green">Feline</span> Feasts.{" "}
+          </h2>
+          <p className="text-[#4A4A4A] text-xl">
+            Taste the Tale: Where Every Dish Narrates Culinary Mastery and
+            Passionate Craftsmanship for Your Feline Friends.
+          </p>
+          <button className="bg-green font-semibold btn text-white px-8 py-3 rounded-full">
+            Order Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
